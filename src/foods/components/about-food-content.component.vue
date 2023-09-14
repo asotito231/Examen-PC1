a66bea62e31ba843931bbe353979e0262cf1c1d5
Andres Soto <u202116113@upc.edu.pe>
2023-09-13 19:02:53 -0500

    feat: added toolbar

---
+++ src/foods/components/about-food-content.component.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <pv-toolbar class="bg-primary">
+      <template #start>
+        <h1>About User</h1>
+      </template>
+    </pv-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about-user"
+}
+</script>
+
+<style scoped>
+
+</style>
