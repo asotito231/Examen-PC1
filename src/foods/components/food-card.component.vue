e183d5035e8e9d1bbbd33ba4613a9073e237cc13
Andres Soto <u202116113@upc.edu.pe>
2023-09-13 19:10:45 -0500

    feat: added food card component

---
+++ src/foods/components/food-card.component.vue
@@ -0,0 +1,28 @@
+<template>
+  <pv-card>
+    <template #content>
+      <p class="flex justify-content-center">Name: {{food.generic_name}}</p>
+      <p class="flex justify-content-center">Categories: {{food.categories}}</p>
+      <p class="flex justify-content-center">Stores: {{food.stores}}</p>
+      <div class="flex justify-content-center">
+        <img :alt="food.generic_name" :src="food.image_front_url">
+      </div>
+      <div class="flex justify-content-center">
+        <img :alt="food.generic_name" :src="food.image_ingredients_url">
+      </div>
+      <p class="flex justify-content-center">Categories imported: {{food.categories_imported}}</p>
+    </template>
+  </pv-card>
+</template>
+
+
+<script>
+export default {
+  name: "food-card",
+  props: {
+    food: null,
+  }
+}
+</script>
+<style scoped>
+</style>
