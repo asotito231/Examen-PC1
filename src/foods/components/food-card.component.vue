7b10834304b693af0f85dd38eed397abd3e0d25a
Andres Soto <u202116113@upc.edu.pe>
2023-09-13 19:15:38 -0500

    feat: imported necessary information

--- src/foods/components/food-card.component.vue
+++ src/foods/components/food-card.component.vue
@@ -1,16 +1,16 @@
 <template>
   <pv-card>
     <template #content>
-      <p class="flex justify-content-center">Name: {{food.generic_name}}</p>
-      <p class="flex justify-content-center">Categories: {{food.categories}}</p>
-      <p class="flex justify-content-center">Stores: {{food.stores}}</p>
+      <h1 class="flex justify-content-center">Name: {{food.generic_name}}</h1>
+      <h2 class="flex justify-content-center">Categories: {{food.categories}}</h2>
+      <h2 class="flex justify-content-center">Stores: {{food.stores}}</h2>
       <div class="flex justify-content-center">
         <img :alt="food.generic_name" :src="food.image_front_url">
       </div>
       <div class="flex justify-content-center">
         <img :alt="food.generic_name" :src="food.image_ingredients_url">
       </div>
-      <p class="flex justify-content-center">Categories imported: {{food.categories_imported}}</p>
+      <h3 class="flex justify-content-center">Categories imported: {{food.categories_imported}}</h3>
     </template>
   </pv-card>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import PrimeVue from "primevue/config";
+
+//App Theme
+import 'primevue/resources/themes/md-light-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+//Add PrimeFlex
+import 'primeflex/primeflex.css';
+
+//Add Components
+import Card from "primevue/card";
+import Button from "primevue/button";
+import Toolbar from "primevue/toolbar";
+import Toast from "primevue/toast";
+
+createApp(App)
+    .use(PrimeVue, {ripple: true})
+    .component('pv-card', Card)
+    .component('pv-button', Button)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-toast', Toast)
+    .mount('#app')
