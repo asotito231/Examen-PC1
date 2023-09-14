2bb490ecac559e9aede3951fa8916e995c35b830
Andres Soto <u202116113@upc.edu.pe>
2023-09-13 19:01:01 -0500

    feat: added footer

---
+++ src/foods/components/footer-content.component.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="grid bg-primary mt-4 p-4 align-content-start">
+    <div class="col-12 ml-3 align-items-center justify-content-center">
+      <p>Copyright © 2023 RESTFoods, inc All rights reserved</p>
+    </div>
+    <div class="col-12 ml-3 mt-3 align-items-center justify-content-center">
+      <p>Developed by: Andrés Eduardo Soto Kong Requena. Code: U202116113</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer-content"
+}
+</script>
+<style scoped>
+</style>
